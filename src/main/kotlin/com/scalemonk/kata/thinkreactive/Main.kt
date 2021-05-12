973db476e652425cd53e933a9ff1d02221c831e4
Lucas Marino <lucas.marino@wildlifestudios.com(opens in new tab)>
2021-05-12 17:59:33 -0300

    empty main

---
+++ src/main/kotlin/com/scalemonk/kata/thinkreactive/Main.kt
@@ -0,0 +1,13 @@
+package com.scalemonk.kata.thinkreactive
+
+import io.reactivex.Observable
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            
+        }
+    }
+}
